3b62ba71a25ffaeebed03cd9e246ed9372b9412b
Cypkka228 <antonershov12340@mail.com>
2026-06-19 05:35:22 +0500

    Fix circular import: models must be imported before views

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from app import views
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'секретный-ключ-замените'
+app.config['SECRET_KEY'] = 'dev-key-123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -19,8 +18,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'user_login'
 login_manager.login_message = 'Пожалуйста, войдите.'
 
+# ВАЖНО: models импортируется ДО views, иначе будет circular import
 from app import models, views
 
 app.config['SECURE_REFERRER_POLICY'] = 'strict-origin-when-cross-origin'
-
-app.config['KINESCOPE_API_KEY'] = '0727b8d9-00a0-47fb-89ad-0789827360c8'
